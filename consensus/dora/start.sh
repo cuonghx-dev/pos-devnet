8f2b7ee2d7cc27280cca40357e872f5d557107c1
cuonghx <cuong1181998@gmail.com>
2026-02-21 13:53:01 +0700

    Fix config

--- consensus/dora/start.sh
+++ consensus/dora/start.sh
@@ -1,6 +1,7 @@
+
 docker run -d \
   --name cl-dora \
   -p 8080:8080 \
-  -v "$(pwd)/dora/explorer-config.yaml:/explorer-config.yaml" \
+  -v "$(pwd)/consensus/dora/explorer-config.yaml:/explorer-config.yaml" \
   pk910/dora-the-explorer:v1.17.0 \
   -config=/explorer-config.yaml
